bcc210020259861f5e06dd93ce3506d21af052f0
Rodney J. Dyer <rjdyer@vcu.edu>
2022-08-16 16:26:16 -0400

    Create packages.R

---
+++ packages.R
@@ -0,0 +1,14 @@
+# These are the first set of code you'll be seeing this semester.  
+
+#  The following is a collection (the c() part) of library names 
+#    that we will need for this class.  
+package_list <- c( 
+  "tidyverse", "gt", "gtExtras", "reactable",
+  "quarto", "rmarkdown", "gtsummary", 
+  "sf", "skimr", "tidyr" 
+  )
+
+# This function installs each of these packages on your machine.
+install.packages( package_list, type="binary" )
+
+
